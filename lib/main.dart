808a8047b6e5bc1e68e51cfdc91edb601ac39e92
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-11-29 00:17:22 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mobx_udemy_flutter/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.deepPurpleAccent,
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.deepPurpleAccent,
+          selectionColor: Colors.deepPurpleAccent,
+          selectionHandleColor: Colors.deepPurpleAccent,
+          
+        ),
+        scaffoldBackgroundColor: Colors.deepPurpleAccent,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
